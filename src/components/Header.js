28946ab684131c1231e6a33b80f4267075537412
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-12 11:51:00 +0300

    Additional CSS Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <div className='Header'>
-        <h2 className='Header-comp1'>A Quiz App</h2>
+        <h2 className='Header-comp1'><Link to= "/Home" className='Heading'>A Quiz App</Link></h2>
         <ul className='Header-comp2'>
-            <li><Link to="/quiz"> Start Quiz</Link></li>
+            <li><Link to="/quiz"className='Link'> Start Quiz</Link></li>
             <li><a>Quiz Results</a></li>
             <li><a>Portfolio</a></li>
             <li><a>Github</a></li>
