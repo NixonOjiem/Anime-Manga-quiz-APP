ae7dbe626e82764df3c3c3f599e7b9f30ebde49a
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-12 09:38:47 +0300

    React dom installed and components working  working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import QuizScreen from './pages/QuizScreen';
+import StartScreen from './pages/StartScreen';
 import ButtonForQuiz from './components/ButtonForQuiz';
 import Header from './components/Header';
 import IntroductoryContent from './components/IntroductoryContent';
@@ -7,9 +10,9 @@ import QuizGenerator from './components/QuizGenerator';
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <IntroductoryContent />
-      <ButtonForQuiz /> */}
+      <ButtonForQuiz /> 
       <QuizGenerator />
     </div>
   );
